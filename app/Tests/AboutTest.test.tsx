26f415f638716800e77e0e79f48859ef841ac20f
BaoPun <baophungus@gmail.com>
2025-07-08 05:12:08 -0700

    Added initial test cases to About page.

--- app/Tests/AboutTest.test.tsx
+++ app/Tests/AboutTest.test.tsx
@@ -2,7 +2,38 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import HomePage from '../Pages/HomePage';
 import MarioKartPage from '../Pages/MarioKart'; 
 import ScoreTrackerPage from '../Pages/ScoreTracker';
+import AboutPage from '../Pages/AboutPage';
 import NotFoundPage from '../Pages/404Page';
 import { MemoryRouter, Routes, Route, Router } from 'react-router';
 import userEvent from '@testing-library/user-event';
 import { vi } from 'vitest';
+
+// Test suite on the about page
+describe('On the About page', () => {
+    // Clicking on the 'Mario Kart' area of the navigation bar will navigate to the Mario Kart page
+    test('navigates to the Mario Kart upon clicking on it', () => {
+        render(
+            <MemoryRouter initialEntries={['/About']}>
+                <Routes>
+                    <Route path="/About" element={<AboutPage />} />
+                    <Route path="/MarioKart" element={<MarioKartPage />} />
+                </Routes>
+            </MemoryRouter>
+        );
+
+        // Click the Mario Kart area, looking for a style of 18px
+        //fireEvent.click(screen.getAllByText('Mario Kart').);
+        for(let i = 0; i < screen.getAllByText('Mario Kart').length; i++){
+            if(screen.getAllByText('Mario Kart')[i].style.fontSize === '18px'){
+                fireEvent.click(screen.getAllByText('Mario Kart')[i]);
+                break;
+            }
+        }
+
+        // Assert that 'mario kart' is rendered on the new page
+        //await expect(document.title).toEqual('Mario Kart');
+        expect(screen.getAllByText(/competitive mario kart/i).length > 0).toBe(true);
+        //expect(document.title).toBe('Mario Kart');
+        console.log('Document title: ' + document.title);
+    });
+});
