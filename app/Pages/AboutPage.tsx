04845539c1ed9b64b80d123808f1baa55d6914e9
BaoPun <baophungus@gmail.com>
2025-07-08 14:22:50 -0700

    Added more content to the About Me page

--- app/Pages/AboutPage.tsx
+++ app/Pages/AboutPage.tsx
@@ -2,6 +2,7 @@ import styles from '../Styles/style.module.css';
 import { Link } from 'react-router';
 import  Navigation  from '../Components/Navigation';
 import type { Route } from "./+types/home";
+import { useState, useEffect } from 'react';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +12,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function AboutMePage(){
+    const [displayDate, setDisplayDate] = useState(new Date());
+
+    // Update the displayed time only once.
+    useEffect(() => {
+        // Set a 1 second interval
+        const interval = setInterval(() => {
+            setDisplayDate(new Date());
+        }, 1000);
+
+        // Clean up on unmount
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className={styles.aboutContainer}>
             {/* Add title at the top  */}
@@ -27,11 +41,12 @@ export default function AboutMePage(){
                 <p>If you want to add me in League of Legends, it's <strong>Nindoge#KEKW</strong> on the NA server.</p><br/>
                 <p>As for competitive Mario Kart, there actually exists a Competitive Mario Kart community.<br/>You can learn more by clicking on the Mario Kart section on the left.</p>
             </div>
-            <br/><br/><br/>
+            <br/><br/>
             {/* Thinking of adding a timeline of my career so far */}
             <div className={styles['aboutCareer']}>
-                <h1>Career</h1>
-                <p>I graduated from Oregon State University on December 20th, 2020 with a bachelor's of science in Computer Science.<br/>I also minored in Mathematics.</p><br/>
+                <h1>Career Timeline</h1>
+                <p>I graduated from Oregon State University on December 20th, 2020 with a Bachelor's of Science (BS) in Computer Science.<br/>I also minored in Mathematics.</p><br/>
+                <p>As of right now ({displayDate.toLocaleString()}), I am currently pursuing my Master's Degree (specifically, Master's of Engineering, or MEng for short) at Oregon State University.</p><br/>
                 <p>After my graduation, I joined Revature - a coding bootcamp that helps place software engineers with another company.</p>
                 <p>Via Revature, I joined Infosys for 2 years from May 2021 to May 2023.<br/>This required me to travel to South Carolina for 2 years.</p><br/>
                 <p>I was initially working as WCS support for Adidas DC1 and DC2, but I got promoted to WMS support after my first year.</p>
@@ -40,6 +55,7 @@ export default function AboutMePage(){
                 <p>As WMS support, I ensure that WMS operations and tasks are properly maintained.  Any troubles with warehouse tasks via WMS are redirected to me.<br/>Since most of WCS messaging comes from WMS, one of my responsibilities was to monitor messaging flow to and from WCS.</p><br/>
                 <p>After my time with Infosys, I returned back home to Portland, Oregon.  A month later, I began freelancing as a software engineering tutor on Wyzant.</p>
                 <p>To this day, I still remain as a freelancing tutor.  However, I have also developed multiple projects during this time.  See the "Projects" section.</p>
+                <p>If you are interested in seeing my tutoring profile, <Link to='https://www.wyzant.com/tutors/baop80' target='_blank' style={{'color': 'cyan'}}>Click here.</Link></p>
             </div>
             <br/><br/><br/>
             {/* Then, looking to add some future aspirations */}
